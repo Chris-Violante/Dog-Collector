a8f41677b49d58a24f5cd023d1426d836eccec5f
Christopher Violante <christopherviolante@Christophers-MacBook-Pro.local>
2020-06-22 09:28:14 -0600

    add,edit, working on delete

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Dog
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 
 
@@ -25,4 +25,15 @@ def dog_detail(request, dog_id):
 class DogCreate(CreateView):
     model = Dog
     fields = '__all__'
+
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
+
 
